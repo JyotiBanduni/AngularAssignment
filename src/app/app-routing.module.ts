4fe0c4d85e35593c79952255e4ad935da0a90544
Jyoti <Jyoti.banduni@gmail.com>
2022-05-06 18:07:54 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ImgThumbnailComponent } from './img-thumbnail/img-thumbnail.component';
+import { ImgComponent } from './img/img.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:ImgComponent},
+{path:'thumb',component:ImgThumbnailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+
 })
+
+
 export class AppRoutingModule { }
